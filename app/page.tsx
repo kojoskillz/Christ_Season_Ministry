4f1f115c4c1ca570df16c3e9a7593e3a3a9ef0e2
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2025-12-09 01:51:45 +0000

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import '../app/globals.css';
+import Homepage from './Components/Homepage/Homepage';
+import Navbar from './Components/Navbar/Navbar';
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Homepage />
+    </>
+  );
+}
